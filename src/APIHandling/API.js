cadd4c45e4cec0a07ac7a2e5461093450a795eb1
razi-king <raziinsane@gmail.com>
2025-07-11 18:03:19 +0530

    Fixed The Bug

--- src/APIHandling/API.js
+++ src/APIHandling/API.js
@@ -1,20 +1,20 @@
 import axios from "axios"
 
 //Fixed The Bugs
-const API=import.meta.env.mainAPI+"/Emp";
+const API=import.meta.env.VITE_mainAPI+"/Emp";
 export const getUser=()=> axios.get(`${API}/emp`);
 export const createUser=(data)=> axios.post(`${API}/saveEmp`,data);
 export const deleteEmployee = (id)=>axios.delete(`${API}/delete/${id}`);
 export const getEmpById = (id)=> axios.get(`${API}/getEmpById/${id}`);
 export const updateEmp = (id,data)=> axios.put(`${API}/update/${id}`,data);
-const API2 = import.meta.env.mainAPI+"/Qualif"
+const API2 = import.meta.env.VITE_mainAPI+"/Qualif"
 export const getQualification=()=> axios.get(`${API2}/getQualif`);
 export const createQualification =(data)=> axios.post(`${API2}/saveQualif`,data);
 export const deleteQualification = (id)=>axios.delete(`${API2}/deleteQualif/${id}`);
 export const getQualifById = (id)=> axios.get(`${API2}/getQualifById/${id}`);
 export const updateQualif = (id,data) => axios.put(`${API2}/updateQualif/${id}`,data);
 // export const loginUser = (data)=> axios.
-const API3 = import.meta.env.mainAPI+"/dept"
+const API3 = import.meta.env.VITE_mainAPI+"/dept"
 export const getDepartment = () => axios.get(`${API3}`);
 export const createDepartment = (data) => axios.post(`${API3}`,data);
 export const deleteDepartment = (id) => axios.delete(`${API3}/${id}`);
