95edbe9431153e3f20b26021200bdd9b246846ec
razi-king <raziinsane@gmail.com>
2025-07-11 17:44:27 +0530

    Update The Apis

--- src/APIHandling/API.js
+++ src/APIHandling/API.js
@@ -1,19 +1,19 @@
 import axios from "axios"
 
-const API="https://host-practice.onrender.com/Emp"
+const API=import.meta.env+"/Emp";
 export const getUser=()=> axios.get(`${API}/emp`);
 export const createUser=(data)=> axios.post(`${API}/saveEmp`,data);
 export const deleteEmployee = (id)=>axios.delete(`${API}/delete/${id}`);
 export const getEmpById = (id)=> axios.get(`${API}/getEmpById/${id}`);
 export const updateEmp = (id,data)=> axios.put(`${API}/update/${id}`,data);
-const API2 = "https://host-practice.onrender.com/Qualif"
+const API2 = import.meta.env+"/Qualif"
 export const getQualification=()=> axios.get(`${API2}/getQualif`);
 export const createQualification =(data)=> axios.post(`${API2}/saveQualif`,data);
 export const deleteQualification = (id)=>axios.delete(`${API2}/deleteQualif/${id}`);
 export const getQualifById = (id)=> axios.get(`${API2}/getQualifById/${id}`);
 export const updateQualif = (id,data) => axios.put(`${API2}/updateQualif/${id}`,data);
 // export const loginUser = (data)=> axios.
-const API3 = "https://host-practice.onrender.com/dept"
+const API3 = import.meta.env+"/dept"
 export const getDepartment = () => axios.get(`${API3}`);
 export const createDepartment = (data) => axios.post(`${API3}`,data);
 export const deleteDepartment = (id) => axios.delete(`${API3}/${id}`);
